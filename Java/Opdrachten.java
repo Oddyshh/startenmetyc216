609d3e5a770e6dfeb455d203b979942afde3039b
Oddyshh <53621671+Oddyshh@users.noreply.github.com>
2021-06-08 20:49:25 +0200

    Opdracht 7 - Lingo - Lingo.spelen doesnt work

--- Java/Opdrachten.java
+++ Java/Opdrachten.java
@@ -16,8 +16,10 @@ public class Opdrachten {
         //Auto auto = new Auto();
         //Monteur monteur = new Monteur();
         //monteur.repair(auto.fix, auto.onderdelen);
-        SortString sortstring = new SortString();
-        sortstring.Sort();
+        //SortString sortstring = new SortString();
+        //sortstring.Sort();
+        Lingo lingo = new Lingo();
+        lingo.spelen();
     }
 }
 
@@ -113,5 +115,62 @@ class SortString {
         String sortedString = new String(charArray);
         System.out.println("Unsorted input: " + input);
         System.out.println("Sorted input: " + sortedString);
+    }
+}
+
+class Lingo {
+    Scanner scan = new Scanner(System.in);
+    String woord = "fiets";
+    String aantalLettersString;
+    int aantalLetters;
+
+    void aantalLetters() {
+        for (int x = 1; x < woord.length() + 1; x++) {
+            aantalLetters += 1;
+            aantalLettersString += "_";
+        }
+        System.out.println("Het woord bevat " + aantalLetters + " letters: ");
+        System.out.println(aantalLettersString);
+    }
+
+
+    void spelen() {
+        char[] resultWoord = new char[woord.length()];
+        char[] resultArray = new char[woord.length()];
+        String spelerKeuze = new String();
+        char[] spelerArray = spelerKeuze.toCharArray();
+        
+        for (int i = 0; i < woord.length(); i++) {
+            resultArray[i] = '2';
+        }
+        
+        this.aantalLetters();
+
+        while (!Arrays.equals(resultArray, spelerArray)) {
+            System.out.println("Kies een woord van " + aantalLetters + " letters: ");
+            spelerKeuze = scan.nextLine();
+            
+            
+            if (spelerKeuze.length() != aantalLettersString.length()) {
+                this.spelen();
+            } else {
+                for (int i = 0; i < spelerKeuze.length(); i++) {
+                    String letter = Character.toString(spelerKeuze.charAt(i));
+                    if (spelerKeuze.charAt(i) == woord.charAt(i)) {
+                        resultArray[i] = '2';
+                        resultWoord[i] = spelerKeuze.charAt(i);
+                    } else if (spelerKeuze.charAt(i) != woord.charAt(i) &&  woord.contains(letter)) {
+                        resultArray[i] = '1';
+                        resultWoord[i] = '_';
+                    } else {
+                        resultArray[i] = '0';
+                        resultWoord[i] = '_';
+                    }
+                
+                }
+                System.out.println(Arrays.toString(resultArray));
+                System.out.println(Arrays.toString(resultWoord));
+            }
+        }
     }
 }
